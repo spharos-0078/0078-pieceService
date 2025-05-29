a3555ceb4592fab43f1e6224ffe77e8fb1515c55
LeeYeongin <youngin5757@gmail.com>
2025-05-29 13:57:43 +0900

    initial commit

---
+++ src/main/java/com/pieceofcake/piece_service/PieceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pieceofcake.piece_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pieceofcake/piece_service/PieceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pieceofcake.piece_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
