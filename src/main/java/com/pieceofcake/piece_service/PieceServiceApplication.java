a5c4e14a80db1d4f63e76654633dd153914cebff
Eunseo <kendyjenny@naver.com>
2025-06-11 10:42:50 +0900

    [Feat]: CICD 배포 자동화

--- src/main/java/com/pieceofcake/piece_service/PieceServiceApplication.java
+++ src/main/java/com/pieceofcake/piece_service/PieceServiceApplication.java
@@ -2,10 +2,11 @@ package com.pieceofcake.piece_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PieceServiceApplication {
 
