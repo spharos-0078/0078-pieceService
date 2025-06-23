b77e9360c7f38b659dcf697a36a59c81054aafd8
Eunseo <kendyjenny@naver.com>
2025-06-23 12:13:13 +0900

    [Fix]: 예치금 입금/차감 로직 수정

--- src/main/java/com/pieceofcake/piece_service/trade/infrastructure/feign/dto/CreateMoneyRequestFeignDto.java
+++ src/main/java/com/pieceofcake/piece_service/trade/infrastructure/feign/dto/CreateMoneyRequestFeignDto.java
@@ -6,26 +6,37 @@ import lombok.Getter;
 @Getter
 public class CreateMoneyRequestFeignDto {
 
-    private Long price;
-    private String moneyHistoryType;
+    private String memberUuid;
+    private Long amount;
+    private String historyType;
+    private Boolean isPositive;
 
     @Builder
-    public CreateMoneyRequestFeignDto(Long price, String moneyHistoryType) {
-        this.price = price;
-        this.moneyHistoryType = moneyHistoryType;
+    public CreateMoneyRequestFeignDto(
+            Long amount, String historyType,
+            Boolean isPositive, String memberUuid
+    ) {
+        this.amount = amount;
+        this.historyType = historyType;
+        this.isPositive = isPositive;
+        this.memberUuid = memberUuid;
     }
 
-    public static CreateMoneyRequestFeignDto buy(Long totalPrice) {
+    public static CreateMoneyRequestFeignDto buy(String memberUuid, Long totalPrice) {
         return CreateMoneyRequestFeignDto.builder()
-                .price(totalPrice)
-                .moneyHistoryType("FRACTION_BUY")
+                .memberUuid(memberUuid)
+                .amount(totalPrice)
+                .isPositive(false)
+                .historyType("FRACTION_BUY")
                 .build();
     }
 
-    public static CreateMoneyRequestFeignDto sell(Long totalPrice) {
+    public static CreateMoneyRequestFeignDto sell(String memberUuid, Long totalPrice) {
         return CreateMoneyRequestFeignDto.builder()
-                .price(totalPrice)
-                .moneyHistoryType("FRACTION_SELL")
+                .memberUuid(memberUuid)
+                .amount(totalPrice)
+                .isPositive(true)
+                .historyType("FRACTION_SELL")
                 .build();
     }
 
