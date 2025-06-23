a004b93441883742c9ad3fbf74ca8aacc8669bf8
Eunseo <kendyjenny@naver.com>
2025-06-23 13:10:09 +0900

    [Fix]: Payment Feign Client 호출값 수정

--- src/main/java/com/pieceofcake/piece_service/trade/infrastructure/feign/dto/CreateMoneyRequestFeignDto.java
+++ src/main/java/com/pieceofcake/piece_service/trade/infrastructure/feign/dto/CreateMoneyRequestFeignDto.java
@@ -3,37 +3,52 @@ package com.pieceofcake.piece_service.trade.infrastructure.feign.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class CreateMoneyRequestFeignDto {
 
-    private String memberUuid;
     private Long amount;
     private String historyType;
     private Boolean isPositive;
+    private String moneyHistoryDetail;
+    private String bankName;
+    private String accountNumber;
+    private String accountHolderName;
+    private String paymentUuid;
+    private LocalDateTime paymentTime;
+    private String paymentMethod;
+    private String paymentStatus;
 
     @Builder
     public CreateMoneyRequestFeignDto(
-            Long amount, String historyType,
-            Boolean isPositive, String memberUuid
+            Long amount, String historyType, Boolean isPositive, String moneyHistoryDetail,
+            String bankName, String accountNumber, String accountHolderName, String paymentUuid,
+            LocalDateTime paymentTime, String paymentMethod, String paymentStatus
     ) {
         this.amount = amount;
         this.historyType = historyType;
         this.isPositive = isPositive;
-        this.memberUuid = memberUuid;
+        this.moneyHistoryDetail = moneyHistoryDetail;
+        this.bankName = bankName;
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.paymentUuid = paymentUuid;
+        this.paymentTime = paymentTime;
+        this.paymentMethod = paymentMethod;
+        this.paymentStatus = paymentStatus;
     }
 
-    public static CreateMoneyRequestFeignDto buy(String memberUuid, Long totalPrice) {
+    public static CreateMoneyRequestFeignDto buy(Long totalPrice) {
         return CreateMoneyRequestFeignDto.builder()
-                .memberUuid(memberUuid)
                 .amount(totalPrice)
                 .isPositive(false)
                 .historyType("FRACTION_BUY")
                 .build();
     }
 
-    public static CreateMoneyRequestFeignDto sell(String memberUuid, Long totalPrice) {
+    public static CreateMoneyRequestFeignDto sell(Long totalPrice) {
         return CreateMoneyRequestFeignDto.builder()
-                .memberUuid(memberUuid)
                 .amount(totalPrice)
                 .isPositive(true)
                 .historyType("FRACTION_SELL")
